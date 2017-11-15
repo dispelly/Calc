3be2a942f6f94639761a4c86823e2771f2c96c4c
Sergey <dispelly@mail.ru>
2017-11-15 22:40:58 +0400

    using stack

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,32 +1,65 @@
 import java.util.StringTokenizer;
+import java.util.Stack;
 
 
 public class Calculator {
 
-    public double calculate(String str) {
-        double num1, num2, answer = 0;
-        char operand;
-        StringTokenizer st = new StringTokenizer(str, "+-*/",true);
-        while (st.hasMoreTokens()) {
-            num1 = Double.parseDouble(st.nextToken());
-            operand = st.nextToken().charAt(0);
-            num2 = Double.parseDouble(st.nextToken());
-            switch (operand) {
-                case '+':
-                    answer = num1 + num2;
-                    break;
-                case '-':
-                    answer = num1 - num2;
-                    break;
-                case '/':
-                    answer = num1 / num2;
-                    break;
-                case '*':
-                    answer = num1 * num2;
-                    break;
-            }
+    private boolean isNum(String currSymb) {
+        try {
+            Double.parseDouble(currSymb);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        return answer;
     }
 
+   private boolean isOp(char c) {
+        if (c=='+' || c=='-' || c=='*' || c=='/')
+            return true;
+        else return false;
+    }
+
+
+    public double calculate(String str) {
+        Stack<Double> stack = new Stack<Double>();
+        double num1 = 0, num2 = 0, answer = 0;
+        char operand;
+        String sN;
+        StringTokenizer st = new StringTokenizer(str, "+-*/", true);
+        while (st.hasMoreTokens()) {
+
+                sN = st.nextToken();
+
+                if (isNum(sN)) {
+                    num1 = Double.parseDouble(sN);
+                    stack.push(num1);
+                } else if (isOp(sN.charAt(0))) {
+                    if (stack.size() < 2) {
+                        System.out.println("Недостаточно операндов для операции " + sN);
+                    }
+                    num1 = stack.pop();
+                    num2 = stack.pop();
+                    operand = sN.charAt(0);
+                    switch (operand) {
+                        case '+':
+                            answer = num1 + num2;
+                            break;
+                        case '-':
+                            answer = num1 - num2;
+                            break;
+                        case '/':
+                            answer = num1 / num2;
+                            break;
+                        case '*':
+                            answer = num1 * num2;
+                            break;
+
+                    }
+                    stack.push(answer);
+                } else System.out.println("Неверные данные");
+
+            }
+
+        return stack.pop();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,18 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Введите арифметическое выражение(можно испольновать знаки '+' '-' '*' '/' и знаки скобок '(' ')'");
         String str;
         double rez;
-        Scanner sc = new Scanner(System.in);
-        str = sc.nextLine();
 
-       Calculator calc=new Calculator();
-        rez= calc.calculate(str);
-        System.out.println(rez);
+            Scanner sc = new Scanner(System.in);
+            str = sc.nextLine();
+            Calculator calc = new Calculator();
+            rez = calc.calculate(str);
+            System.out.println(rez);
+
     }
-
-
 }
 
 
