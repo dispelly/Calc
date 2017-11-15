922b323b35d710aee735895271c62a292efd980c
Sergey <dispelly@mail.ru>
2017-11-15 12:09:56 +0400

    Delete readline()

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Введите арифметическое выражение(можно испольновать знаки '+' '-' '*' '/' и знаки скобок '(' ')'");
+        String str;
+        double rez;
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+
         Calculator calc=new Calculator();
-        calc.readline();
+        rez= calc.calculate(str);
+        System.out.println("Результат: "+rez);
     }
 
 
