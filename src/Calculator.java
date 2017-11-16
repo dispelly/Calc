7c5e4796be0bb03a1899fe3dcbee97cdeea495b4
Sergey <dispelly@mail.ru>
2017-11-16 11:54:52 +0400

    add exceptions and some javadoc

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,10 @@ import java.util.Stack;
 
 
 public class Calculator {
-
+    /**
+     * Метод проверяет, является ли строка числом
+     * @param currSymb String
+     */
     private boolean isNum(String currSymb) {
         try {
             Double.parseDouble(currSymb);
@@ -12,22 +15,30 @@ public class Calculator {
             return false;
         }
     }
-
+    /**
+     * Метод проверяет, является ли символ оператором
+     * @param c char
+     */
    private boolean isOp(char c) {
         if (c=='+' || c=='-' || c=='*' || c=='/')
             return true;
         else return false;
     }
 
-
-    public double calculate(String str) {
+    /**
+     * главный метод подсчета выражения
+     * запись операторов в стек
+     * @param str String
+     * @return double
+     */
+    public double calculate(String str) throws Exception{
         Stack<Double> stack = new Stack<Double>();
         double num1 = 0, num2 = 0, answer = 0;
         char operand;
         String sN;
         StringTokenizer st = new StringTokenizer(str, "+-*/", true);
         while (st.hasMoreTokens()) {
-
+            try {
                 sN = st.nextToken();
 
                 if (isNum(sN)) {
@@ -35,29 +46,31 @@ public class Calculator {
                     stack.push(num1);
                 } else if (isOp(sN.charAt(0))) {
                     if (stack.size() < 2) {
-                        System.out.println("Недостаточно операндов для операции " + sN);
-                    }
-                    num1 = stack.pop();
-                    num2 = stack.pop();
-                    operand = sN.charAt(0);
-                    switch (operand) {
-                        case '+':
-                            answer = num1 + num2;
-                            break;
-                        case '-':
-                            answer = num1 - num2;
-                            break;
-                        case '/':
-                            answer = num1 / num2;
-                            break;
-                        case '*':
-                            answer = num1 * num2;
-                            break;
+                        throw new Exception("Недостаточно операндов для операции " + sN);
+                        }
+                        num1 = stack.pop();
+                        num2 = stack.pop();
+                        operand = sN.charAt(0);
+                        switch (operand) {
+                            case '+':
+                                answer = num1 + num2;
+                                break;
+                            case '-':
+                                answer = num1 - num2;
+                                break;
+                            case '/':
+                                answer = num1 / num2;
+                                break;
+                            case '*':
+                                answer = num1 * num2;
+                                break;
 
-                    }
-                    stack.push(answer);
-                } else System.out.println("Неверные данные");
-
+                        }
+                        stack.push(answer);
+                    } /*else System.out.println("Неверные данные");*/
+                } catch (Exception e) {
+                throw new Exception("Неверные данные в выражении");
+                }
             }
 
         return stack.pop();
--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,19 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args)throws Exception {
         System.out.println("Введите арифметическое выражение(можно испольновать знаки '+' '-' '*' '/' и знаки скобок '(' ')'");
         String str;
         double rez;
-
+        try {
             Scanner sc = new Scanner(System.in);
             str = sc.nextLine();
             Calculator calc = new Calculator();
             rez = calc.calculate(str);
             System.out.println(rez);
-
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
 
