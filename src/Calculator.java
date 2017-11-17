f4425df40c43aa0018d931926956d8312b912c43
Sergey <dispelly@mail.ru>
2017-11-17 18:13:48 +0400

    using reverse Polish notation

--- src/Calculator.java
+++ src/Calculator.java
@@ -26,53 +26,82 @@ public class Calculator {
     }
 
     /**
-     * главный метод подсчета выражения
-     * запись операторов в стек
-     * @param str String
-     * @return double
+     * приоритет операции
+     * @param op char
+     * @return byte
      */
-    public double calculate(String str) throws Exception{
-        Stack<Double> stack = new Stack<Double>();
-        double num1 = 0, num2 = 0, answer = 0;
-        char operand;
+    private static byte opPrior(char op) {
+        switch (op) {
+            case '/':
+            case '*':
+                return 2;
+            case '+':
+            case '-':
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    /**
+     * главный метод подсчета выражения
+     * используем обратную польскую запись
+     * @param str String
+     * @return String
+     */
+    public double calculate(String str) throws Exception {
+        Stack<Double> stack = new Stack<Double>(); // записываем цифры
+        Stack<Character> op = new Stack<Character>(); // записываем опрераторы
         String sN;
-        StringTokenizer st = new StringTokenizer(str, "+-*/", true);
+        StringTokenizer st = new StringTokenizer(str, "+-*/()", true);
         while (st.hasMoreTokens()) {
-            try {
-                sN = st.nextToken();
-
-                if (isNum(sN)) {
-                    num1 = Double.parseDouble(sN);
-                    stack.push(num1);
+            sN = st.nextToken();
+                if (sN.charAt(0) == '(')
+                    op.push('(');
+                else if (sN.charAt(0) == ')') {
+                    while (op.peek() != '(')
+                        resultOp(stack, op.pop());
+                    op.pop();
                 } else if (isOp(sN.charAt(0))) {
-                    if (stack.size() < 2) {
-                        throw new Exception("Недостаточно операндов для операции " + sN);
-                        }
-                        num1 = stack.pop();
-                        num2 = stack.pop();
-                        operand = sN.charAt(0);
-                        switch (operand) {
-                            case '+':
-                                answer = num1 + num2;
-                                break;
-                            case '-':
-                                answer = num1 - num2;
-                                break;
-                            case '/':
-                                answer = num1 / num2;
-                                break;
-                            case '*':
-                                answer = num1 * num2;
-                                break;
-
-                        }
-                        stack.push(answer);
-                    } /*else System.out.println("Неверные данные");*/
-                } catch (Exception e) {
-                throw new Exception("Неверные данные в выражении");
+                    while (!op.empty() && opPrior(op.peek()) >= opPrior(sN.charAt(0)))
+                        resultOp(stack, op.pop());
+                    op.push(sN.charAt(0));
+                } else {
+                    if (isNum(sN))
+                    stack.push(Double.parseDouble(sN));
                 }
-            }
+
+        }
+        while (!op.isEmpty())
+            resultOp(stack, op.pop());
 
         return stack.pop();
+    }
+
+    /**
+     * метод подсчета двух чисел
+     *@param stack Stack<Double> передаем стек чисел
+     *@param operator char передаем оператор
+     */
+   private void resultOp(Stack<Double> stack, char operator) {
+        double answer = 0;
+        double num1 = stack.pop();
+        double num2 = stack.pop();
+        switch (operator) {
+            case '+':
+                answer = num1 + num2;
+                break;
+            case '-':
+                answer = num1 - num2;
+                break;
+            case '/':
+                answer = num1 / num2;
+                break;
+            case '*':
+                answer = num1 * num2;
+                break;
+
+            }
+        stack.push(answer);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
 
